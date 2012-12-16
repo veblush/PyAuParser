b316988f45fed26f0694836cf9b5860b7441bc3d
Esun Kim <veblush+git@gmail.com>
2012-12-16 10:40:31 +0900

    Remove CR from auparser for linux

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PyAuParser',
-    version='0.53',
+    version='0.53.1',
     author="Esun Kim",
     author_email='veblush+git_at_gmail.com',
     url='https://github.com/veblush/PyAuParser',
